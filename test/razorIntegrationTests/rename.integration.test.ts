232db692f3e32bba9c8b33862a0406f1d5b5f3c9
Andrew Hall (METAL) <andrha@microsoft.com>
2023-11-13 15:01:36 -0800

    Start of tests

---
+++ test/razorIntegrationTests/rename.integration.test.ts
@@ -0,0 +1,47 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as path from 'path';
+import * as vscode from 'vscode';
+import { describe, beforeAll, afterAll, test, expect } from '@jest/globals';
+import testAssetWorkspace from './testAssets/testAssetWorkspace';
+import * as integrationHelpers from '../integrationTests/integrationHelpers';
+
+describe(`Razor Rename ${testAssetWorkspace.description}`, function () {
+    beforeAll(async function () {
+        if (!integrationHelpers.isRazorWorkspace(vscode.workspace)) {
+            return;
+        }
+
+        await integrationHelpers.openFileInWorkspaceAsync(path.join('Pages', 'Counter.razor'));
+        await integrationHelpers.activateCSharpExtension();
+    });
+
+    afterAll(async () => {
+        await testAssetWorkspace.cleanupWorkspace();
+    });
+
+    test('Rename currentCount', async () => {
+        if (!integrationHelpers.isRazorWorkspace(vscode.workspace)) {
+            return;
+        }
+
+        const activeDocument = vscode.window.activeTextEditor?.document.uri;
+        if (!activeDocument) {
+            throw new Error('No active document');
+        }
+
+        const renameEdit = <vscode.WorkspaceEdit>await vscode.commands.executeCommand(
+            'vscode.executeDocumentRenameProvider',
+            activeDocument,
+            {
+                line: 10,
+                character: 17,
+            }
+        );
+
+        expect(renameEdit).toBeDefined();
+    });
+});
